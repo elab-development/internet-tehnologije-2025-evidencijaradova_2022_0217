999148c3159fda154cab93b7e378afea64f08548
Lazar Obrenic <lakiobr@gmail.com>
2026-02-09 18:48:46 +0100

    Fix

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,18 +1,3 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
 export const prisma = new PrismaClient();
